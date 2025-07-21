240e2350bd059e2c89bdad8ab1d3d5483458d702
skal28 <105671798+skal28@users.noreply.github.com>
2025-07-21 22:03:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ async def startup_db_client():
 
 app.include_router(products.router, prefix="/products", tags=["Products"])
 app.include_router(orders.router, prefix="/orders", tags=["Orders"])
+
+@app.get("/")
+async def root():
+    return {"message": "HROne backend is live!"}
